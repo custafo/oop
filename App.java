fa20dbba125e25cf4c741c5137deebd88fb12654
GUSTAVO PEREIRA OLIVEIRA LIMA <gustavo.00000844518@unicap.br>
2022-10-11 12:11:31 +0000

    programção orientada a objetos

--- App.java
+++ App.java
@@ -1,13 +1,13 @@
 public class App {
     public static void main(String[] args){
-        Person person1 = new Person("Pepeu" , 18);
+        Person person1 = new Person("Pepeu" , 18); // instances
         Person person2 = new Person("Guga", 19);
 
-        System.out.println(person1);
+        System.out.println(person1); // call tostring object1
         // sout person1.name + ":" +person1.age;
-        System.out.println(person2);
+        System.out.println(person2); 
 
-        boolean isEquals = person1 == person2;
+        boolean isEquals = person1.equals(person2);
         System.out.println(isEquals);
     }
 }
